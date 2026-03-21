c0d786824e9ee86b69cab5f0344c486a71d56cfd
Saqlain Malik <saqlainmalik@maxifysolution.com>
2026-03-21 22:33:35 +0530

    by malik

--- adserabackend/models/authmodel.js
+++ adserabackend/models/authmodel.js
@@ -1,31 +1,35 @@
 const mongoose = require('mongoose');
 
-const userschema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userschema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true
     },
-    mobile:{
-        type:String,
-        required:true
+    mobile: {
+      type: String,
+      required: true
     },
-    role:{
-        enum:['admin','user'],
-        default:'user',
-        type:String,
-        required:true
+    role: {
+      enum: ['admin', 'user'],
+      default: 'user',
+      type: String,
+      required: true
     },
-    password:{
-        type:String,
-        required:true
-        
+    password: {
+      type: String,
+      required: true
     }
-})
+  },
+  {
+    timestamps: true 
+  }
+);
 
-const User = mongoose.model('users',userschema)
+const User = mongoose.model('users', userschema);
 
 module.exports = User;
